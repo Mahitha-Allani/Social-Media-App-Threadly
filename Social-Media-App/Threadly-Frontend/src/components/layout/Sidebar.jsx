5b711083c65a28ecc0801ca47dd07516c342805e
Mahitha <allanimahitha@gmail.com>
2026-05-25 19:34:10 +0530

    added socket notifications

--- Social-Media-App/Threadly-Frontend/src/components/layout/Sidebar.jsx
+++ Social-Media-App/Threadly-Frontend/src/components/layout/Sidebar.jsx
@@ -65,7 +65,6 @@ export default function Sidebar() {
             </button>
           ))}
         </nav>
-//
         <button className={btn.sidebarPost} onClick={() => navigate('/?new_post=' + Date.now())}>
           <span className="lg:hidden text-xl leading-none">+</span>
           <span className="hidden lg:inline text-[15px]">+ New Post</span>
